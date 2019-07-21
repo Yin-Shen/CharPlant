2bb0af6d1eee25076a7761a5124942e1ac8b0412
shen yin <1490025927@qq.com>
2019-07-21 14:02:01 +0800

    Update cat_predict.sh

--- src/cat_predict.sh
+++ src/cat_predict.sh
@@ -2,17 +2,18 @@
 cd ../de_novo_prediction
 MAXINDEX=`find . -name "*.py" |wc -l`
 File=`ls whole_predict_fasta*.txt 2>/dev/null |wc -w`
-while [ $File -le ${MAXINDEX} ]
+while (( $File < ${MAXINDEX} ))
 do
    File=`ls whole_predict_fasta*.txt 2>/dev/null |wc -w`
-   if [ $File = ${MAXINDEX} ]
-     then
-      for i in `ls whole_predict_fasta*.txt |sort -V`
-      do
-        echo "File name is: $i";
-        cat $i >> whole_predict;
-      done
-   fi
-   break
+   sleep 10
 done
+if [ $File = ${MAXINDEX} ]
+ then
+  for i in `ls whole_predict_fasta*.txt |sort -V`
+  do
+     echo "File name is: $i";
+     cat $i >> whole_predict;
+  done
+fi
+
 
